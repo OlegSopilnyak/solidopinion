ff4508fd0a5cdbc2636ae87aed6704595412f04e
Oleg Sopilnyak <oleg.sopilnyak@gmail.com>
2017-08-16 23:27:02 +0300

    Some improvements

--- src/main/java/oleg/sopilnyak/test1/Sum.java
+++ src/main/java/oleg/sopilnyak/test1/Sum.java
@@ -7,20 +7,22 @@ import java.nio.ByteOrder;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 import java.util.Arrays;
-import java.util.Set;
-import java.util.concurrent.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.stream.Collectors;
 import java.util.stream.LongStream;
 
 /**
  * Task to test sum from file
  */
 public class Sum implements Runnable {
-    private static final int CHUNK_SIZE = 1_000_000;
+//    private static final int CHUNK_SIZE = 4;
+        private static final int CHUNK_SIZE = 1_000_000;
     private static final int BUFFER_SIZE = 8192;
     private volatile long total = 0;
 
+    private final ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(2);
     private String pathToFile = "target/classes/test1/simple.txt";
-    private final Set<ScheduledFuture> activeThreads = new ConcurrentSkipListSet<>();
 
     public Sum() {
     }
@@ -52,31 +54,20 @@ public class Sum implements Runnable {
     }
 
     private void calculateParallel(File dataFile) throws IOException {
-        long chunks = dataFile.length() / CHUNK_SIZE;
-        ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(2);
         executor.setMaximumPoolSize(10);
-
+        final long chunks = dataFile.length() / CHUNK_SIZE;
         // launch the threads
-        LongStream.range(0, chunks).forEach(i -> {
-            final ScheduledFuture future = executor.schedule(() -> {
-                try {
-                    calculateSingleChunk(dataFile, i * CHUNK_SIZE);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }, 0, TimeUnit.MILLISECONDS);
-            // collect the futures
-            activeThreads.add(future);
-        });
-        // suspend call
-        activeThreads.forEach(f -> {
-            try {
-                // waiting for Thread ends
-                f.get();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
+        LongStream.range(0, chunks).mapToObj(i ->
+            CompletableFuture.runAsync(() -> {
+                        try {
+                            calculateSingleChunk(dataFile, i * CHUNK_SIZE);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }, executor)
+        ).collect(Collectors.toList())
+                // waiting for all calculation done
+                .forEach(f -> f.join());
     }
 
     // calculate chunk of file
@@ -97,21 +88,22 @@ public class Sum implements Runnable {
         }
     }
 
-    private void calculateChunk(FileChannel channel, long startPoint, long maxBytes) throws IOException {
-        long red = startPoint;
+    private void calculateChunk(FileChannel channel, long startPoint, long chunkSize) throws IOException {
+        long offset = startPoint;
+        long lastChannelPos = startPoint + chunkSize;
         final int[] payloadInt = new int[BUFFER_SIZE / 4];
         do {
-            final long read = Math.min(BUFFER_SIZE, maxBytes - red);
-            final MappedByteBuffer buf = channel.map(FileChannel.MapMode.READ_ONLY, red, read);
+            final long read = Math.min(BUFFER_SIZE, lastChannelPos - offset);
+            final MappedByteBuffer buf = channel.map(FileChannel.MapMode.READ_ONLY, offset, read);
             final int payloadSize = Math.min(buf.remaining(), BUFFER_SIZE) / 4;
 
             buf.order(ByteOrder.LITTLE_ENDIAN).asIntBuffer().get(payloadInt, 0, payloadSize);
-            int payload = Arrays.stream(payloadInt, 0, payloadSize - 1).sum();
+            int payload = Arrays.stream(payloadInt, 0, payloadSize).sum();
             if (total + payload > Long.MAX_VALUE) {
                 return;
             }
             total += payload;
-            red += read;
-        } while (red < maxBytes);
+            offset += read;
+        } while (offset < lastChannelPos);
     }
 }
